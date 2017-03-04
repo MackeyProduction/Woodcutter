712c91ffaa4b308fe1c6d0ffe2cb3ec9d5df697a
Til Anheier <tmtipps-help@gmx.de>
2017-03-04 17:09:11 +0100

    GUI design added.

--- src/woodcutter/GUI.java
+++ src/woodcutter/GUI.java
@@ -1,7 +1,33 @@
 package woodcutter;
 
+import javax.swing.*;
+
 /**
- * Created by Til Anheier on 03.03.2017.
+ * Created by Til Anheier on 04.03.2017.
  */
 public class GUI {
+    private JPanel panel1;
+    private JLabel treeLabel;
+    private JComboBox cbTree;
+    private JCheckBox chbPowerchopping;
+    private JCheckBox chbIntelligentChopping;
+    private JTabbedPane jTPMain;
+    private JPanel jPMain;
+    private JPanel jPManual;
+    private JPanel jPAntiban;
+    private JLabel locationLabel;
+    private JLabel distanceLabel;
+    private JLabel currentDistanceLabel;
+    private JSlider slDistance;
+    private JLabel availableTreesLabel;
+    private JList listAvailableTrees;
+    private JList listAddedTrees;
+    private JButton btnRemoveFromList;
+    private JButton btnAddToList;
+    private JCheckBox chbHopWorlds;
+    private JSpinner spMaxPlayers;
+    private JLabel lblMaxPlayers;
+    private JCheckBox chbAntiMod;
+    private JButton btnStart;
+    private JButton btnIntelligentChoppingInfo;
 }
